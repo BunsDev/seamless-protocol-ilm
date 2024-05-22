d89a819c84b77427d7344691da2f459f69b6e9b3
NouDaimon <peric.iason@gmail.com>
2024-05-22 13:32:51 +0300

    sync: OZ with local

--- keeper/src/actions/checks.js
+++ keeper/src/actions/checks.js
@@ -82,9 +82,7 @@ async function hasEPSDecreased(store, strategy) {
 
         return {
             strategyAddress: strategy.address,
-            hasEPSDecreased: ethers.BigNumber.from(prevEPS.toString()).lt(
-                ethers.BigNumber.from(currentEPS.toString())
-            ),
+            hasEPSDecreased: ethers.BigNumber.from(prevEPS.toString()).gt(ethers.BigNumber.from(currentEPS.toString())),
             prevEPS: prevEPS,
             currentEPS: currentEPS,
         };
